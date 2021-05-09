39b3bdcf5918b1dea9eaa212850ffccf12dd087b
Jonathan Boiser <jboiser@gmail.com>
2021-05-08 19:58:23 -0700

    Add FS recording

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
+import * as FullStory from '@fullstory/browser';
 import App from './App.vue'
 import './index.css'
+
+FullStory.init({ orgId: import.meta.env.VITE_APP_FULLSTORY_ORG as string });
+
+const app = createApp(App);
+app.config.globalProperties.$FullStory = FullStory;
 
 createApp(App).mount('#app')
